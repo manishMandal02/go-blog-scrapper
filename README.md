f401547eeca9458e6d1a9e846723a095a7f4afc6
Manish Mandal <manishmandalj@gmail.com>
2024-07-31 23:59:37 +0530

    chore: add health check api endpoint

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ### Go web scrapper
+ 
--- cmd/scrapper/main.go
+++ cmd/scrapper/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -16,6 +17,20 @@ func main() {
 	mux.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
 	mux.HandleFunc("/", handlers.GetHomePageHandler)
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		w.WriteHeader(http.StatusOK)
+
+		res := make(map[string]string)
+
+		res["status"] = "ok"
+
+		json.NewEncoder(w).Encode(res)
+
+		fmt.Fprintf(w, "{'status':'ok'}")
+	})
 
 	mux.HandleFunc("/scrapper/{blog}", handlers.StartScrapper)
 
